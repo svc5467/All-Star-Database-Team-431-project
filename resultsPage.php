9b1d3549546ebc6890af61f1815d81ed713996e6
Brent <riva.brent@gmail.com>
2016-04-20 22:40:46 -0400

    WORKING (but terrible)
    ugly, ugly, search results and printed fields for all items in our
    database.

---
+++ resultsPage.php
@@ -0,0 +1,45 @@
+<?php
+	$dbh = new PDO("sqlite:c:/xampp/htdocs/allStarDB/allstar.sqlite");
+	$itemRows = $dbh->query("SELECT title, buy_it_now_price, description FROM Items");
+	
+	if(!$itemRows) {
+			die('Invalid query: ' . mysql_error());
+		}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Search Results</title>
+	<meta name="description" content="Search Query Results">
+	<meta name="author" content="Brent Riva">
+	
+	<link rel="stylesheet" href="css/styles.css?v=1.0">
+	<!--[if lt IE9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<script src="js/scripts.js"></script>
+	<h1>Search results:</h1>
+	<br>
+	<br>
+	<?php
+						
+		while($row = $itemRows->fetch(PDO::FETCH_ASSOC)) {
+			echo '<div><ul>';
+			foreach($row as $key => $var)
+			{
+				echo '<li>' . $key . ': '. $var;
+			}
+			#TODO: figure-out URL convention for each individual item. needed to get to item page.
+			#		likely pass ID thru URL then parsed for dynamimc pages
+			echo '</ul></div>';
+		}
+?>
+
+</body>
+</html>
