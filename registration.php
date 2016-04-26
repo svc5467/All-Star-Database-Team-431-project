983c1df2557232bdabe09cd94aa134080c4676c2
Brent <riva.brent@gmail.com>
2016-04-26 02:46:00 -0400

    Tested & partially fixed
    Got the javascript to work, but the PDO queries are not. Must fix how I
    query the database. This will likely be done by changing what i send to
    the registrationScript.php function.

--- registration.php
+++ registration.php
@@ -1,8 +1,18 @@
 <?php
-	$seller_id = uniq_id();
-	$address_id = uniq_id();
+	$seller_id = uniqid();
+	$unique_id = uniqid();
+	$sum = 0;
+	
+	// convert the uniqueid to an integer
+	for($i = 0; $i < strlen($unique_id); $i++)
+	{
+		if((ord($unique_id[$i])-96) < 0) // integer character
+			$sum += intval($unique_id[$i]) * (10** $i);
+		else
+			$sum += (ord($unique_id[$i])-96) * (10**$i);
+	}
+	$address_id = $sum;
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,11 +29,9 @@
 </head>
 
 <body>
-	<script src="js/scripts.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<h1>User Registration:</h1>
-	<br>
-	<br>
-	<form id="u_reg" class="registration_form" action="" method="post" name="registration_form">
+	<form id="u_reg" class="registration_form" align="center" action="" method="post" name="registration_form">
 		<ul>
 			<li>
 				<h2>User Information</h2>
@@ -51,7 +59,7 @@
 			</li>
 			<li>
 				<label for="birthday" required>*Birthday:</label>
-				<input title="U" type="date" name="birthday" />
+				<input title="U" type="date" name="birthday" placeholder="YYYY-MM-DD"/>
 			</li>
 			<li>
 				<label for="annual_income">Annual Income:</label>
@@ -77,53 +85,68 @@
 				<input title="A" type="number" name="zipcode" />
 			</li>
 			<li>
-				<button onclick="register()" class="submit" type="submit">Register</button>
+				<input type="button" value="Submit" onclick="register()"/>
 			</li>
 		</ul>	
 	</form>
 	
 	<script type="text/javascript">
-		function register() {
+		function register() {			
+			alert("Register was called");
+			
+			var request;
+			
 			var i = 0;
 			var table = 0;
 			var x = document.getElementById("u_reg");
 			
 			// pull randomly generated IDs from PHP
 			var seller_id = "<?php echo $seller_id; ?>";
-			var address_id = "<?php echo $address_id; ?>";\
+			var address_id = <?php echo $address_id; ?>;
 			
 			// Queries for: Users, Sellers, Addresses (Respectively)
 			var queries = ["INSERT INTO Users(user_id", "INSERT INTO Sellers(seller_id, address_id", "INSERT INTO Addresses (address_id"];
 			var queries2 = [") VALUES (", ") VALUES (", ") VALUES ("];
 			
-			queries2[0] += + seller_id; // "...) VALUES ($seller_id..."
-			queries2[1] += + seller_id + ", " + address_id; // "...) VALUES ($seller_id, $address_id..."
-			queries2[2] += + seller_id; // "...) VALUES ($seller_id..."
+			queries2[0] += "'" + seller_id + "'"; // "...) VALUES ($seller_id..."
+			queries2[1] += "'" + seller_id + "', '" + address_id + "'"; // "...) VALUES ($seller_id, $address_id..."
+			queries2[2] += "'" + seller_id + "'"; // "...) VALUES ($seller_id..."
 					
 			// pull user info from the form.
 			for(i = 0; i < x.length; i++)
 			{
-				if(x.elements[i].title = "U") // add to user fields	
+				if(x.elements[i].title == "U") // add to user fields	
 					table = 0;
-				else if(x.elements[i].title = "S") // add to seller fields. all required, no need to null check.
+				else if(x.elements[i].title == "S") // add to seller fields. all required, no need to null check.
 					table = 1;
-				else // send to address table
+				else if (x.elements[i].title == 'A') // send to address table
 					table = 2;
-					
-				if (x.elements[i].value != "")
+				else  // look at the three cases. USA! woo. haha.
+					table = -1 // error, dont add to query.
+				
+				if (x.elements[i].value != "" && table >= 0)
 				{
 					queries[table] += ", " + x.elements[i].name;
-					queries2[table] += ", " + x.elements[i].value;
+					queries2[table] += "', '" + x.elements[i].value;
 				}
 			}
 			
 			// create queries
-			val Uquery = queries[0] + queries2[0] + ");";
-			val Squery = queries[1] + queries2[1] + ");";
-			val Aquery = queries[2] + queries2[2] + ");";
+			var Uquery = queries[0] + queries2[0] + "');";
+			var Squery = queries[1] + queries2[1] + "');";
+			var Aquery = queries[2] + queries2[2] + "');";
 			
-			$.post('registrationScript.php', {Uquery, Squery, Aquery}, function(data){
-				alert(data); // hopefully is 111. (1 row for each query)
+			request = $.ajax({
+				url: "http://localhost:8080/AllStarDB/registrationScript.php",
+				type: "post",
+				data: {q1:Uquery, q2:Squery, q3:Aquery},
+				
+				success: function(html){
+					alert(html);
+				},
+				failure: function(html){
+					alert(html);
+				}
 			});
 		}
 	</script>
--- registrationScript.php
+++ registrationScript.php
@@ -1,12 +1,12 @@
 <?php
-	$Uquery = $_POST["Uquery"];
-	$Squery = $_POST["Squery"];
-	$Aquery = $_POST["Aquery"];
+	$Uquery = $_POST["q1"];
+	$Squery = $_POST["q2"];
+	$Aquery = $_POST["q3"];
 	
 	$dbh = new PDO("sqlite:c:/xampp/htdocs/allStarDB/allstar.sqlite");
-	$AResult = $dbh->exec($Aquery);
-	$SResult = $dbh->exec($Squery);
-	$UResult = $dbh->exec($Uquery);
+	$AResult = $dbh->query($Aquery);
+	//$SResult = $dbh->exec($Squery);
+	//$UResult = $dbh->exec($Uquery);
 	
-	echo $AResult . $SResult . $UResult
+	echo $dbh->errorCode();
 ?>
